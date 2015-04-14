f8f7c83e45f40cae506385d210f1e5e7cf64c63f
Shan Shen <shanshen@shans-mac-pro.ad.bcm.edu>
2015-04-14 11:47:14 -0500

    Dimitri helped clean up some connectivity classes

--- +connectivity/Distance.m
+++ +connectivity/Distance.m
@@ -8,40 +8,24 @@ dist_y  :   double             # distance in y coordinate
 %}
 
 classdef Distance < dj.Relvar & dj.AutoPopulate
-	properties
-        popRel = connectivity.CellTestedPair & connectivity.CellPosition
+    properties
+        popRel = pro(connectivity.CellTestedPair, connectivity.ConnectMembership*connectivity.CellPosition, 'count(*)->n') & 'n=2'
     end
     
     methods(Access=protected)
-
-		function makeTuples(self, key)
-            % fetch cell pairs
-            key_pair = fetch(connectivity.CellTestedPair & key);
-            
-            key_cell_from = fetch(connectivity.ConnectMembership & key_pair & 'role="from"');
-            key_cell_to = fetch(connectivity.ConnectMembership & key_pair & 'role="to"');
-            
-            if ~isempty(key_cell_from) && ~isempty(key_cell_to)            
-                [x1,y1] = fetch1(connectivity.CellPosition & key_cell_from, 'cell_pos_x', 'cell_pos_y');
-                [x2,y2] = fetch1(connectivity.CellPosition & key_cell_to, 'cell_pos_x', 'cell_pos_y');
-            else
-                keys = fetch(connectivity.ConnectMembership & key_pair & 'role="EC"');
-                if length(keys)~=2
-                    assert('Connection is not complete!');
-                else
-                   	[x1,y1] = fetch1(connectivity.CellPosition & keys(1), 'cell_pos_x', 'cell_pos_y');
-                    [x2,y2] = fetch1(connectivity.CellPosition & keys(2), 'cell_pos_x', 'cell_pos_y');
-                end
-            end
-            
+        
+        function makeTuples(self, key)
+            [x,y,role] = fetchn(connectivity.CellPosition*connectivity.ConnectMembership & key, ...
+                'cell_pos_x', 'cell_pos_y', 'role');
+            assert(numel(x)==2 && (all(strcmp(role,'EC')) || ~any(strcmp(role,'EC'))), 'invalid pair')
             
             % compute distances
-            key.dist_x = abs(x2-x1);
-            key.dist_y = abs(y2-y1);
-            key.distance = sqrt((y2-y1)^2+(x2-x1)^2);
+            key.dist_x = abs(diff(x));
+            key.dist_y = abs(diff(y));
+            key.distance = sqrt(diff(x)^2+diff(y)^2);
             
             self.insert(key)
-		end
-	end
-
+        end
+    end
+    
 end
